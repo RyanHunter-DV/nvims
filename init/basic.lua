7284bcd0f1e6f4d7daa74c3f92828d739f2a68cb
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-01-06 21:04:16 -0500

    remove softtab for personal use

--- init/basic.lua
+++ init/basic.lua
@@ -11,5 +11,7 @@ vim.cmd ([[ set foldmarker=##{{{,##}}} ]])
 vim.cmd ([[au BufRead,BufNewFile *.rh set filetype=ruby]])
 vim.cmd ([[au FileType ruby setlocal shiftwidth=4 ]])
 vim.cmd ([[au FileType * setlocal fo-=o ]])
+vim.cmd ([[au FileType * set softtabstop=0 ]])
+vim.cmd ([[au FileType * set noexpandtab ]])
 vim.cmd ([[au BufRead,BufNewFile *.svh set filetype=systemverilog]])
 
