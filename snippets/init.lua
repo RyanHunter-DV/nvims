25e8df395eff5c0c0f1dff88154243239c532523
ryanhunter <ryanhunter@rhTestPC.dynamic>
2023-06-15 09:49:00 -0400

    up

--- snippets/init.lua
+++ snippets/init.lua
@@ -1,2 +1,4 @@
-require('snippets.uvm')
+-- if vim.api.nvim_buf_get_option(0,'filetype')=='ruby' then
+-- end
 require('snippets.ruby')
+require('snippets.uvm')
--- snippets/ruby.lua
+++ snippets/ruby.lua
@@ -6,7 +6,7 @@ local i = ls.insert_node
 local c = ls.choice_node
 local sn = ls.snippet_node
 local r = ls.restore_node
-local uvm = {}
+-- local uvm = {}
 
 local function copy(args)
 	return args[1]
