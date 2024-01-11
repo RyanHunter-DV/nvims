59e96239068cf107a6765114dab87b716885a1e2
ryanhunter <ryanhunter30479@outlook.com>
2024-01-11 21:31:32 +0800

    up

--- configs/ll-config.lua
+++ configs/ll-config.lua
@@ -1,40 +1,45 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+local s ={}
+s.setup=function(home)
+	package.path=package.path..string.format(";%s/plugins/lualine/lua/?.lua",home);
+	require('lualine').setup {
+	  options = {
+	    icons_enabled = true,
+	    theme = 'auto',
+	    component_separators = { left = '', right = ''},
+	    section_separators = { left = '', right = ''},
+	    disabled_filetypes = {
+	      statusline = {},
+	      winbar = {},
+	    },
+	    ignore_focus = {},
+	    always_divide_middle = true,
+	    globalstatus = false,
+	    refresh = {
+	      statusline = 1000,
+	      tabline = 1000,
+	      winbar = 1000,
+	    }
+	  },
+	  sections = {
+	    lualine_a = {'mode'},
+	    lualine_b = {'branch', 'diff', 'diagnostics'},
+	    lualine_c = {'filename'},
+	    lualine_x = {'encoding', 'fileformat', 'filetype'},
+	    lualine_y = {'progress'},
+	    lualine_z = {'location'}
+	  },
+	  inactive_sections = {
+	    lualine_a = {},
+	    lualine_b = {},
+	    lualine_c = {'filename'},
+	    lualine_x = {'location'},
+	    lualine_y = {},
+	    lualine_z = {}
+	  },
+	  tabline = {},
+	  winbar = {},
+	  inactive_winbar = {},
+	  extensions = {}
+	}
+end
+return s;
--- configs/plugin.lua
+++ configs/plugin.lua
@@ -2,11 +2,11 @@ local s={};
 s.setup=function(home)
 	require('cmp-config').setup(home);
 	require('theme-config')
-	require('ts-config')
-	require('ll-config')
+	require('ts-config').setup(home);
+	require('ll-config').setup(home);
 	-- require('init.tmux-config')
 	-- require('init.lsp-config')
-	--TODO, require('luasnip-config')
+	require('luasnip-config').setup(home);
 	require('tterm-config').setup(home);
 end
 return s;
